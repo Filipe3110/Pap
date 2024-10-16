f360528466bd623bb67c7d228aeb79ef3b226078
Filipe3110 <filipe.p.nunes2006@gmail.com>
2024-10-16 15:22:03 +0100

    s

--- khaos/Scrips/player.gd
+++ khaos/Scrips/player.gd
@@ -1,21 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@export var SPEED := 200.0
+@export var grav := 10.0
+@export var JUMP_FORCE := -300.0  # Força do salto, negativo para ir para cima
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Aplica gravidade quando o personagem não está no chão
+	if !is_on_floor():
+		velocity.y += grav
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = 0  # Reset da gravidade no chão
 
-	move_and_slide()
+	# Movimento para a direita
+	if Input.is_action_pressed("right"):
+		velocity.x = SPEED if is_on_floor() else SPEED * 1.3
+		$AnimatedSprite2D.flip_h = false
+	# Movimento para a esquerda
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED if is_on_floor() else -SPEED * 1.3
+		$AnimatedSprite2D.flip_h = true
+	# Nenhum movimento lateral
+	else:
+		velocity.x = 0
+
+	# Salto
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_FORCE  # Aplica a força do salto
+
+	move_and_slide()  # Necessário para mover e aplicar física no personagem
