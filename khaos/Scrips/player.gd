1a996d30db6685115ea164a7869f33ad3d7632e9
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-01-12 17:19:02 +0000

    Movimentaçoes feitas

--- khaos/Scrips/player.gd
+++ khaos/Scrips/player.gd
@@ -4,13 +4,16 @@ const VELOCIDADE = 300.0
 const FORCA_PULO = -400.0
 var esta_pulando := false
 var esta_atacando := false
+var esta_abaixando := false
+var esta_bloqueando := false
 
-@onready var sprite_animado = $anim as AnimatedSprite2D  
-@onready var barra_de_vida = $BarraDeVida 
-
+@onready var sprite_animado = $anim as AnimatedSprite2D
+@onready var barra_de_vida = $BarraDeVida
 var combo = ["soco_direita", "soco_esquerda", "uppercut"]
-var contcombo: int = 0  
-var tempultclick: int = 0
+var Baixo_combo = ["baixo_soco_direita", "baixo_soco_esquerda"]
+
+var contcombo: int = 0
+var contcombo_baixo: int = 0
 
 func _ready():
 	if barra_de_vida == null:
@@ -23,17 +26,32 @@ func _physics_process(delta: float) -> void:
 		esta_pulando = true
 	else:
 		esta_pulando = false
-	
+
 	if Input.is_action_just_pressed("up") and is_on_floor() and not esta_atacando:
 		velocity.y = FORCA_PULO
 		esta_pulando = true
-	
-	if Input.is_action_just_pressed("attack") and not esta_atacando:
-		iniciar_ataque()
+
+	if Input.is_action_just_pressed("attack") and not esta_bloqueando:
+		if Input.is_action_pressed("down") and is_on_floor():
+			iniciar_ataque_baixo()
+		else:
+			iniciar_ataque()
+
+	if Input.is_action_pressed("down") and is_on_floor() and not esta_pulando and not esta_atacando:
+		esta_abaixando = true
+		sprite_animado.play("Crunsh")
+	else:
+		esta_abaixando = false
+
+	if Input.is_action_pressed("block") and is_on_floor() and not esta_pulando and not esta_atacando:
+		esta_bloqueando = true
+		sprite_animado.play("block")
+	else:
+		esta_bloqueando = false
 
 	var direcao := Input.get_axis("left", "right")
 
-	if not esta_atacando:
+	if not esta_atacando and not esta_abaixando and not esta_bloqueando:
 		if direcao != 0:
 			velocity.x = direcao * VELOCIDADE
 			sprite_animado.scale.x = direcao
@@ -49,22 +67,35 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func iniciar_ataque() -> void:
-	esta_atacando = true
-	sprite_animado.play("soco_direita")
+	if not esta_atacando:
+		esta_atacando = true
+		contcombo = 0
+	else:
+		contcombo += 1
+		if contcombo >= combo.size():
+			contcombo = 0
+	sprite_animado.play(combo[contcombo])
 
+func iniciar_ataque_baixo() -> void:
+	if not esta_atacando:
+		esta_atacando = true
+		contcombo_baixo = 0
+	else:
+		contcombo_baixo += 1
+		if contcombo_baixo >= Baixo_combo.size():
+			contcombo_baixo = 0
+	sprite_animado.play(Baixo_combo[contcombo_baixo])
 
 func _quando_animacao_finalizar() -> void:
-	if sprite_animado.animation == "soco_direita":
+	if sprite_animado.animation in combo or sprite_animado.animation in Baixo_combo:
 		esta_atacando = false
-		
-
 
 func receber_dano(dano):
 	if barra_de_vida == null:
 		return
 	if not barra_de_vida.has_method("receber_dano"):
 		return
-	
+
 	barra_de_vida.receber_dano(dano)
 
 func morrer():
--- khaos/Scrips/player_2.gd
+++ khaos/Scrips/player_2.gd
@@ -1,63 +1,88 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_FORCE = -400.0
-var is_jumping := false
-var is_attacking := false
+const VELOCIDADE = 300.0
+const FORCA_PULO = -400.0
+var esta_pulando := false
+var esta_atacando := false
+var esta_abaixando := false
 
-@onready var anim_sprite = $anim as AnimatedSprite2D  
-@onready var barra_de_vida = $BarraDeVida 
+@onready var sprite_animado = $anim as AnimatedSprite2D
+@onready var barra_de_vida = $BarraDeVida
+var combo = ["soco_direita", "soco_esquerda", "uppercut"]
+var Baixo_combo = ["baixo_soco_direita", "baixo_soco_esquerda"]
+
+var contcombo: int = 0
+var contcombo_baixo: int = 0
 
 func _ready():
 	if barra_de_vida == null:
 		print("Erro: Barra de vida não encontrada no nó Player!")
+	sprite_animado.connect("animation_finished", Callable(self, "_quando_animacao_finalizar"))
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		is_jumping = true
+		esta_pulando = true
 	else:
-		is_jumping = false
-	
-	if Input.is_action_just_pressed("up") and is_on_floor() and not is_attacking:
-		velocity.y = JUMP_FORCE
-		is_jumping = true
-	
-	if Input.is_action_just_pressed("attack") and not is_attacking:
-		start_attack()
+		esta_pulando = false
 
-	var direction := Input.get_axis("left", "right")
+	if Input.is_action_just_pressed("up") and is_on_floor() and not esta_atacando:
+		velocity.y = FORCA_PULO
+		esta_pulando = true
 
-	if not is_attacking:
-		if direction != 0:
-			velocity.x = direction * SPEED
-			anim_sprite.scale.x = direction
+	if Input.is_action_just_pressed("attack"):
+		if esta_abaixando:
+			iniciar_ataque_baixo()
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			iniciar_ataque()
 
-		if is_jumping:
-			anim_sprite.play("jump")
-		elif direction != 0:
-			anim_sprite.play("run")
+	if Input.is_action_pressed("down") and is_on_floor() and not esta_pulando and not esta_atacando:
+		esta_abaixando = true
+		sprite_animado.play("Crunsh")
+	else:
+		esta_abaixando = false
+
+	var direcao := Input.get_axis("left", "right")
+
+	if not esta_atacando and not esta_abaixando:
+		if direcao != 0:
+			velocity.x = direcao * VELOCIDADE
+			sprite_animado.scale.x = direcao
 		else:
-			anim_sprite.play("idle")
+			velocity.x = move_toward(velocity.x, 0, VELOCIDADE)
+		if esta_pulando:
+			sprite_animado.play("jump")
+		elif direcao != 0:
+			sprite_animado.play("run")
+		else:
+			sprite_animado.play("idle")
 
 	move_and_slide()
 
-func start_attack() -> void:
-	is_attacking = true
-	anim_sprite.play("attack")
+func iniciar_ataque() -> void:
+	esta_atacando = true
+	contcombo += 1
+	if contcombo >= combo.size():
+		contcombo = 0
+	sprite_animado.play(combo[contcombo])
 
-func _on_animation_finished() -> void:
-	if anim_sprite.animation == "attack":
-		is_attacking = false
-		
+func iniciar_ataque_baixo() -> void:
+	esta_atacando = true
+	contcombo_baixo += 1
+	if contcombo_baixo >= Baixo_combo.size():
+		contcombo_baixo = 0
+	sprite_animado.play(Baixo_combo[contcombo_baixo])
+
+func _quando_animacao_finalizar() -> void:
+	if sprite_animado.animation in combo or sprite_animado.animation in Baixo_combo:
+		esta_atacando = false
+
 func receber_dano(dano):
 	if barra_de_vida == null:
 		return
 	if not barra_de_vida.has_method("receber_dano"):
 		return
-	
+
 	barra_de_vida.receber_dano(dano)
 
 func morrer():
