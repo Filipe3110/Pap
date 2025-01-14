1c1899f3cd73fe1222961a0a29478ec963f6a7fe
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-01-14 00:06:05 +0000

    animation player cheio de bugs

--- khaos/Scrips/player.gd
+++ khaos/Scrips/player.gd
@@ -7,7 +7,7 @@ var esta_atacando := false
 var esta_abaixando := false
 var esta_bloqueando := false
 
-@onready var sprite_animado = $anim as AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 @onready var barra_de_vida = $BarraDeVida
 var combo = ["soco_direita", "soco_esquerda", "uppercut"]
 var Baixo_combo = ["baixo_soco_direita", "baixo_soco_esquerda"]
@@ -18,7 +18,7 @@ var contcombo_baixo: int = 0
 func _ready():
 	if barra_de_vida == null:
 		print("Erro: Barra de vida não encontrada no nó Player!")
-	sprite_animado.connect("animation_finished", Callable(self, "_quando_animacao_finalizar"))
+	animation_player.connect("animation_finished", Callable(self, "_quando_animacao_finalizar"))
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -39,13 +39,13 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("down") and is_on_floor() and not esta_pulando and not esta_atacando:
 		esta_abaixando = true
-		sprite_animado.play("Crunsh")
+		animation_player.play("Crunsh")
 	else:
 		esta_abaixando = false
 
 	if Input.is_action_pressed("block") and is_on_floor() and not esta_pulando and not esta_atacando:
 		esta_bloqueando = true
-		sprite_animado.play("block")
+		animation_player.play("block")
 	else:
 		esta_bloqueando = false
 
@@ -54,15 +54,16 @@ func _physics_process(delta: float) -> void:
 	if not esta_atacando and not esta_abaixando and not esta_bloqueando:
 		if direcao != 0:
 			velocity.x = direcao * VELOCIDADE
-			sprite_animado.scale.x = direcao
+			if (direcao < 0 and scale.x > 0) or (direcao > 0 and scale.x < 0):
+				scale.x *= -1
 		else:
 			velocity.x = move_toward(velocity.x, 0, VELOCIDADE)
 		if esta_pulando:
-			sprite_animado.play("jump")
+			animation_player.play("jump")
 		elif direcao != 0:
-			sprite_animado.play("run")
+			animation_player.play("run")
 		else:
-			sprite_animado.play("idle")
+			animation_player.play("idle")
 
 	move_and_slide()
 
@@ -74,7 +75,7 @@ func iniciar_ataque() -> void:
 		contcombo += 1
 		if contcombo >= combo.size():
 			contcombo = 0
-	sprite_animado.play(combo[contcombo])
+	animation_player.play(combo[contcombo])
 
 func iniciar_ataque_baixo() -> void:
 	if not esta_atacando:
@@ -84,10 +85,10 @@ func iniciar_ataque_baixo() -> void:
 		contcombo_baixo += 1
 		if contcombo_baixo >= Baixo_combo.size():
 			contcombo_baixo = 0
-	sprite_animado.play(Baixo_combo[contcombo_baixo])
+	animation_player.play(Baixo_combo[contcombo_baixo])
 
-func _quando_animacao_finalizar() -> void:
-	if sprite_animado.animation in combo or sprite_animado.animation in Baixo_combo:
+func _quando_animacao_finalizar(anim_name):
+	if anim_name in combo or anim_name in Baixo_combo:
 		esta_atacando = false
 
 func receber_dano(dano):
