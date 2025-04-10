59b3616cbf9eed07ae8407a065ddb34b1350d947
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-04-10 22:45:43 +0100

    Revert "save de volta bd"
    This reverts commit d17f576a75cd3f8bf181f97cd2940ddf60b383ef.

--- khaos/Scripts/UI/Authentication.gd
+++ khaos/Scripts/UI/Authentication.gd
@@ -1,14 +1,20 @@
 extends Control
 
+
 func _ready():
-	if Firebase.Auth == null:
-		Firebase.Firebase.setup()
 	Firebase.Auth.login_succeeded.connect(on_login_succeeded)
+	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
 	Firebase.Auth.login_failed.connect(on_login_failed)
+	Firebase.Auth.signup_failed.connect(on_signup_failed)
 	
 	if Firebase.Auth.check_auth_file():
 		%StateLabel.text = "Logged in"
 		get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
+
+
+func _process(delta):
+	pass
+
 
 func _on_login_button_pressed():
 	var email = %EmailLineEdit.text
@@ -16,9 +22,21 @@ func _on_login_button_pressed():
 	Firebase.Auth.login_with_email_and_password(email, password)
 	%StateLabel.text = "Logging in"
 
+func _on_signup_button_pressed():
+	var email = %EmailLineEdit.text
+	var password = %PasswordLineEdit.text
+	Firebase.Auth.signup_with_email_and_password(email, password)
+	%StateLabel.text = "Singing up"
+
 func on_login_succeeded(auth):
 	print(auth)
 	%StateLabel.text = "Login success!"
+	Firebase.Auth.save_auth(auth)
+	get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
+	
+func on_signup_succeeded(auth):
+	print(auth)
+	%StateLabel.text = "Sign up success!"
 	Firebase.Auth.save_auth(auth)
 	get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
 	
@@ -26,11 +44,8 @@ func on_login_failed(error_code, message):
 	print(error_code)
 	print(message)
 	%StateLabel.text = "Login failed. Error: %s" % message
-
-
-func _on_create_account_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/UI/new_account.tscn")
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+	
+func on_signup_failed(error_code, message):
+	print(error_code)
+	print(message)
+	%StateLabel.text = "Sign up failed. Error: %s" % message
