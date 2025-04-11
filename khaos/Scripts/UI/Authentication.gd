5900de41c3587ae70c3e5e84bac867237c1d8476
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-04-11 16:59:02 +0200

    save log out

--- khaos/Scripts/UI/Authentication.gd
+++ khaos/Scripts/UI/Authentication.gd
@@ -3,17 +3,13 @@ extends Control
 
 func _ready():
 	Firebase.Auth.login_succeeded.connect(on_login_succeeded)
-	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
 	Firebase.Auth.login_failed.connect(on_login_failed)
-	Firebase.Auth.signup_failed.connect(on_signup_failed)
 	
 	if Firebase.Auth.check_auth_file():
 		%StateLabel.text = "Logged in"
 		get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
 
 
-func _process(delta):
-	pass
 
 
 func _on_login_button_pressed():
@@ -22,11 +18,7 @@ func _on_login_button_pressed():
 	Firebase.Auth.login_with_email_and_password(email, password)
 	%StateLabel.text = "Logging in"
 
-func _on_signup_button_pressed():
-	var email = %EmailLineEdit.text
-	var password = %PasswordLineEdit.text
-	Firebase.Auth.signup_with_email_and_password(email, password)
-	%StateLabel.text = "Singing up"
+
 
 func on_login_succeeded(auth):
 	print(auth)
@@ -34,18 +26,18 @@ func on_login_succeeded(auth):
 	Firebase.Auth.save_auth(auth)
 	get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
 	
-func on_signup_succeeded(auth):
-	print(auth)
-	%StateLabel.text = "Sign up success!"
-	Firebase.Auth.save_auth(auth)
-	get_tree().change_scene_to_file("res://Cenas/UI/main.tscn")
+
 	
 func on_login_failed(error_code, message):
 	print(error_code)
 	print(message)
 	%StateLabel.text = "Login failed. Error: %s" % message
 	
-func on_signup_failed(error_code, message):
-	print(error_code)
-	print(message)
-	%StateLabel.text = "Sign up failed. Error: %s" % message
+
+func _on_create_account_pressed() -> void:
+	print("Mudando para cena de criação de conta.")
+	get_tree().change_scene_to_file("res://Cenas/UI/new_account.tscn")
+
+func _on_quit_pressed() -> void:
+	print("Fechando o jogo.")
+	get_tree().quit()
--- khaos/Scripts/UI/tela_inicial.gd
+++ khaos/Scripts/UI/tela_inicial.gd
@@ -1,7 +1,29 @@
 extends Control
 
+var COLLECTION_ID = "khaos_stats"
 
+@onready var Logout = $LogoutScrn
 
+func _ready():
+	
+	Logout.visible = false
+	
+	
+	load_player_data()
+func load_player_data():
+	var auth = Firebase.Auth.auth
+	if auth and auth.localid:  
+		var collection: FirestoreCollection = Firebase.Firestore.collection(COLLECTION_ID)
+		var task: FirestoreTask = collection.get_doc(auth.localid)
+		
+
+		var finished_task: FirestoreTask = await task.task_finished
+		var document = finished_task.document
+		
+		if document and document.doc_fields:
+			if document.doc_fields.has("username"): 
+				$log.text = document.doc_fields.username 
+				
 func _on_modo_história_pressed():
 	get_tree().change_scene_to_file("res://Cenas/Modo_História/Tela_dos_mapas.tscn")
 
@@ -12,7 +34,6 @@ func _on_opções_pressed():
 	get_tree().change_scene_to_file("res://Cenas/controles.tscn")
 
 func _on_sair_pressed():
-	Firebase.Auth.logout()
 	get_tree().quit()
 
 
@@ -21,3 +42,15 @@ func carregar_cena_com_loading(caminho_da_proxima_cena):
 	loading.next_scene = caminho_da_proxima_cena
 	get_tree().root.add_child(loading)
 	queue_free()
+
+
+func _on_log_pressed() -> void:
+	Logout.visible = true
+	
+func _on_no_2_pressed() -> void:
+		Logout.visible = false
+
+
+func _on_yes_2_pressed() -> void:
+	Firebase.Auth.logout()
+	get_tree().change_scene_to_file("res://Cenas/UI/Authentication.tscn")
